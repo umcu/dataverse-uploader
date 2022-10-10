93eb52ea35b6eb749af1506caeb1ac6e66426822
Nico Poppelier <n.poppelier@umcutrecht.nl>
2022-10-10 11:48:47 +0200

    added text for type 2

--- dave/terms.py
+++ dave/terms.py
@@ -1,27 +1,36 @@
 terms = {
-'type_1': {
-  "license": {"name": "CC-BY-4.0", "uri": "http://creativecommons.org/licenses/by/4.0"},
-  "termsOfAccess": "By downloading or otherwise accessing the materials, the downloader represents his/her acceptance of the Terms of Use. ",
-},
-'type_2': {},
-'type_3a': {
-  "termsOfUse": "The standard Data Sharing Agreement (DSA) of the UMC Utrecht must be signed without adjustments. This DSA is in compliance with Dutch law. No costs are involved.",
-  "confidentialityDeclaration": "no",
-  "specialPermissions": "To obtain access to the data, a <a href=\"https://www.umcutrecht.nl/en/data-request-form-umc-utrecht\">request form</a> has to be completed. In addition to a completed request form, a  Data Sharing Agreement (DSA) in line with GDPR regulations and/or a Research Collaboration Agreement (RCA) should be signed before data is shared. Only data requests in line with the Terms of Use will be taken into consideration. ",
-  "restrictions": "See Data Sharing Agreement.",
-  "citationRequirements": "See Data Sharing Agreement.",
-  "conditions": "To access and use the dataset please read the Terms of Use and the Terms of Access.",
-  "disclaimer": "See Data Sharing Agreement.",
-  "termsOfAccess": "By downloading or otherwise accessing the materials, the downloader represents his/her acceptance of the Terms of Use. ",
-},
-'type_3b': {
-  "termsOfUse": "The standard Data Sharing Agreement (DSA) of the UMC Utrecht must be signed without adjustments. This DSA is in compliance with Dutch law. No costs are involved.",
-  "confidentialityDeclaration": "no",
-  "specialPermissions": "To obtain access to the data, a <a href=\"https://www.umcutrecht.nl/en/data-request-form-umc-utrecht\">request form</a> has to be completed. In addition to a completed request form, a  Data Sharing Agreement (DSA) in line with GDPR regulations and/or a Research Collaboration Agreement (RCA) should be signed before data is shared. Only data requests in line with the Terms of Use will be taken into consideration. ",
-  "restrictions": "See Data Sharing Agreement.",
-  "citationRequirements": "See Data Sharing Agreement.",
-  "conditions": "To access and use the dataset please read the Terms of Use and the Terms of Access.",
-  "disclaimer": "See Data Sharing Agreement.",
-  "termsOfAccess": "By downloading or otherwise accessing the materials, the downloader represents his/her acceptance of the Terms of Use. ",
-}
+  'type_1': {
+    "license": {"name": "CC-BY-4.0", "uri": "http://creativecommons.org/licenses/by/4.0"},
+    "termsOfAccess": "By downloading or otherwise accessing the materials, the downloader represents his/her acceptance of the Terms of Use. ",
+  },
+  'type_2': {
+    "termsOfUse": "See UMC Utrecht license.",
+    "confidentialityDeclaration": "no",
+    "specialPermissions": "no",
+    "restrictions": "no",
+    "citationRequirements": "We ask researchers to include an acknowledgment on behalf of the UMC Utrecht. If you use this dataset in a publication, please cite the dataset, and include the following in the citation: author; year; dataset title; dataset DOI; datase version; repository.",
+    "conditions": "To access and use the dataset please read the Terms of Use and the Terms of Access.",
+    "disclaimer": "It is expressly understood that UMC Utrecht does not make any warranties regarding the data and specifically does not warrant or guarantee that the data will be accurate, be merchantable or useful for any particular purpose. Use of the data is at your own risk. UMC Utrecht cannot and shall not be held liable for any claims or damages by you or any third party, in connection with or as a result of the use of data by you.",
+    "termsOfAccess": "By downloading or otherwise accessing the materials, the downloader represents his/her acceptance of the Terms of Use.",
+  },
+  'type_3a': {
+    "termsOfUse": "The standard Data Sharing Agreement (DSA) of the UMC Utrecht must be signed without adjustments. This DSA is in compliance with Dutch law. No costs are involved.",
+    "confidentialityDeclaration": "no",
+    "specialPermissions": "To obtain access to the data, a <a href=\"https://www.umcutrecht.nl/en/data-request-form-umc-utrecht\">request form</a> has to be completed. In addition to a completed request form, a  Data Sharing Agreement (DSA) in line with GDPR regulations and/or a Research Collaboration Agreement (RCA) should be signed before data is shared. Only data requests in line with the Terms of Use will be taken into consideration. ",
+    "restrictions": "See Data Sharing Agreement.",
+    "citationRequirements": "See Data Sharing Agreement.",
+    "conditions": "To access and use the dataset please read the Terms of Use and the Terms of Access.",
+    "disclaimer": "See Data Sharing Agreement.",
+    "termsOfAccess": "By downloading or otherwise accessing the materials, the downloader represents his/her acceptance of the Terms of Use. ",
+  },
+  'type_3b': {
+    "termsOfUse": "The standard Data Sharing Agreement (DSA) of the UMC Utrecht must be signed without adjustments. This DSA is in compliance with Dutch law. No costs are involved.",
+    "confidentialityDeclaration": "no",
+    "specialPermissions": "To obtain access to the data, a <a href=\"https://www.umcutrecht.nl/en/data-request-form-umc-utrecht\">request form</a> has to be completed. In addition to a completed request form, a  Data Sharing Agreement (DSA) in line with GDPR regulations and/or a Research Collaboration Agreement (RCA) should be signed before data is shared. Only data requests in line with the Terms of Use will be taken into consideration. ",
+    "restrictions": "See Data Sharing Agreement.",
+    "citationRequirements": "See Data Sharing Agreement.",
+    "conditions": "To access and use the dataset please read the Terms of Use and the Terms of Access.",
+    "disclaimer": "See Data Sharing Agreement.",
+    "termsOfAccess": "By downloading or otherwise accessing the materials, the downloader represents his/her acceptance of the Terms of Use. ",
+  }
 }
